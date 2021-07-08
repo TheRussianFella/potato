d8857e1f08a80e17a33d858af7f285349d959287
TheRussianFella <maximmanainen@gmail.com>
2021-07-08 23:40:13 +0300

    slave:plist done

--- src/slave/slave_struct.go
+++ src/slave/slave_struct.go
@@ -1,30 +1,14 @@
 package slave
 
 import (
+	"errors"
+	"strconv"
 	"time"
 )
 
-// potat is an interface for objects that could be stored in a PotatoSlave's data storage
-type potat interface {
-	getTimeOfDeath() time.Time
-	getContent(string) string
-	setContent(string, string)
-}
-
-type pstring struct {
-	content     string
-	timeOfDeath time.Time
-}
-
-func (p pstring) getTimeOfDeath() time.Time {
-	return p.timeOfDeath
-}
-
-func (p pstring) getContent(idx string) string {
-	return p.content
-}
-
-func (p pstring) setContent(val string, idx string) {}
+/////////
+// Main structure
+/////////
 
 // PotatoSlave is a class that deals with storing objects of users, assigned
 // to it by a PotatoMaster.
@@ -34,7 +18,8 @@ type PotatoSlave struct {
 	IP   string
 	port string
 
-	STALETIME time.Duration
+	STALETIME  time.Duration
+	DEFAULTTTL time.Duration
 
 	// Functions - a map that holds invocable functions
 	functions map[string]func(string, CommandMessage) ResponseMessage
@@ -49,19 +34,99 @@ type PotatoSlave struct {
 }
 
 // NewSlave creates an instance of a PotatoSlave.
-func NewSlave(IP string, port string, STALETIME time.Duration) *PotatoSlave {
+func NewSlave(IP string, port string, STALETIME time.Duration, DEFAULTTTL time.Duration) *PotatoSlave {
 
 	s := PotatoSlave{
-		IP:        IP,
-		port:      port,
-		STALETIME: STALETIME,
-		storage:   make(map[string]map[string]potat),
-		functions: make(map[string]func(string, CommandMessage) ResponseMessage),
-		stop:      false,
+		IP:         IP,
+		port:       port,
+		STALETIME:  STALETIME,
+		DEFAULTTTL: DEFAULTTTL,
+		storage:    make(map[string]map[string]potat),
+		functions:  make(map[string]func(string, CommandMessage) ResponseMessage),
+		stop:       false,
 	}
 
 	s.functions["GET"] = s.get
 	s.functions["SET"] = s.set
+	s.functions["LGET"] = s.lget
+	s.functions["LSET"] = s.lset
+	s.functions["LPUSH"] = s.lpush
 
 	return &s
+}
+
+/////////
+// Structures that represent data
+/////////
+
+// potat is an interface for objects that could be stored in a PotatoSlave's data storage
+type potat interface {
+	getTimeOfDeath() time.Time
+	getContent(string) (string, error)
+	setContent(string, string) error
+}
+
+///// String
+
+type pstring struct {
+	content     string
+	timeOfDeath time.Time
+}
+
+func (p *pstring) getTimeOfDeath() time.Time {
+	return p.timeOfDeath
+}
+
+func (p *pstring) getContent(idx string) (string, error) {
+	return p.content, nil
+}
+
+func (p *pstring) setContent(val string, idx string) error { return nil }
+
+///// List
+
+type plist struct {
+	list        []string
+	timeOfDeath time.Time
+}
+
+func (p *plist) getTimeOfDeath() time.Time {
+	return p.timeOfDeath
+}
+
+// getContent retrieves a string on idx position of list.
+func (p *plist) getContent(idx string) (string, error) {
+
+	i, err := strconv.Atoi(idx)
+	if err != nil {
+		return "", errors.New("wr")
+	}
+
+	if i >= len(p.list) {
+		return "", errors.New("ou")
+	}
+
+	return p.list[i], nil
+}
+
+// setContent does quite a self explainatory thing. -1 here means a tale of the list.
+func (p *plist) setContent(val string, idx string) error {
+
+	i, err := strconv.Atoi(idx)
+	if err != nil {
+		return errors.New("wr")
+	}
+
+	if i == -1 {
+		p.list = append(p.list, val)
+		return nil
+	}
+
+	if i >= len(p.list) {
+		return errors.New("ou")
+	}
+
+	p.list[i] = val
+	return nil
+
 }
--- src/slave/slave_test.go
+++ src/slave/slave_test.go
@@ -3,6 +3,7 @@ package slave
 import (
 	"encoding/json"
 	"net"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -41,26 +42,32 @@ func TestStale(t *testing.T) {
 }
 */
 
-func TestGetSet(t *testing.T) {
+func newClient(testPort string) (*json.Encoder, *json.Decoder, ResponseMessage) {
+
+	time.Sleep(time.Second)
+	conn, err := net.Dial("tcp", "localhost:"+testPort)
+	if err != nil {
+		panic(err)
+	}
+
+	encoder := json.NewEncoder(conn)
+	decoder := json.NewDecoder(conn)
+
+	var response ResponseMessage
+
+	return encoder, decoder, response
+}
+
+func TestPstring(t *testing.T) {
 
 	// Create a slave
 	testPort := "62554"
-	staleTime := time.Second
-	s := NewSlave("localhost", testPort, staleTime)
+	s := NewSlave("localhost", testPort, time.Second, time.Minute)
 
 	// Client simulator
 	go func(testPort string, s *PotatoSlave, t *testing.T) {
 
-		time.Sleep(time.Second)
-		conn, err := net.Dial("tcp", "localhost:"+testPort)
-		if err != nil {
-			panic(err)
-		}
-
-		encoder := json.NewEncoder(conn)
-		decoder := json.NewDecoder(conn)
-
-		var response ResponseMessage
+		encoder, decoder, response := newClient(testPort)
 
 		mes1 := CommandMessage{
 			Name:      "SET",
@@ -94,6 +101,71 @@ func TestGetSet(t *testing.T) {
 	}(testPort, s, t)
 
 	// Slave code
-	s.StartServing(true)
+	s.StartServing()
 
+}
+
+func TestPlist(t *testing.T) {
+
+	// Create a slave
+	testPort := "62554"
+	s := NewSlave("localhost", testPort, time.Second, time.Minute)
+
+	// Client simulator
+	go func(testPort string, s *PotatoSlave, t *testing.T) {
+
+		encoder, decoder, response := newClient(testPort)
+
+		// Make some insertions
+		for i := 0; i < 5; i++ {
+			mes := CommandMessage{
+				Name:      "LPUSH",
+				Arguments: []string{"mylist", strconv.Itoa(i)},
+			}
+
+			encoder.Encode(mes)
+			decoder.Decode(&response)
+
+			if response.Code != _OK {
+				t.Errorf("Couldn't push")
+			}
+
+			mes = CommandMessage{
+				Name:      "LGET",
+				Arguments: []string{"mylist", strconv.Itoa(i)},
+			}
+
+			encoder.Encode(mes)
+			decoder.Decode(&response)
+
+			if response.Code != _OK {
+				t.Errorf("Couldn't get. Got error message: %s", response.StatusMessage)
+			}
+			if response.Value != strconv.Itoa(i) {
+				t.Errorf("Got wrong value")
+			}
+		}
+
+		// Check that changing at index works
+		mes := CommandMessage{
+			Name:      "LSET",
+			Arguments: []string{"mylist", "3", "10"},
+		}
+		encoder.Encode(mes)
+		decoder.Decode(&response)
+		mes = CommandMessage{
+			Name:      "LGET",
+			Arguments: []string{"mylist", "3"},
+		}
+		encoder.Encode(mes)
+		decoder.Decode(&response)
+		if response.Value != "10" {
+			t.Errorf("Got wrong value after LSET.")
+		}
+
+		s.stop = true
+	}(testPort, s, t)
+
+	// Start serving
+	s.StartServing()
 }
